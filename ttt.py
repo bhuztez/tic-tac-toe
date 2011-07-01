75a4d403d8e0aef6a2baa0661c208f2bb9e89465
bhuztez <bhuztez@gmail.com>
2011-07-01 17:15:24 +0800

    tic-tac-toe logic

--- ttt.py
+++ ttt.py
@@ -16,10 +16,21 @@ try:
     c1 = Telnet("localhost", 9999)
     c2 = Telnet("localhost", 9999)
 
-    c1.write("ping\n")
-    assert c2.read_some() == "ping\n"
-    c2.write("pong\n")
-    assert c1.read_some() == "pong\n"
+    sys.stdout.write('c2 ' + c2.read_some())
+    sys.stdout.write('c1 ' + c1.read_some())
+    c1.write("PONG\r\n4\r\n")
+    sys.stdout.write('c2 ' + c2.read_some())
+    c2.write("PONG\r\n0\r\n")
+    sys.stdout.write('c1 ' + c1.read_some())
+    c1.write("PONG\r\n1\r\n")
+    sys.stdout.write('c2 ' + c2.read_some())
+    c2.write("PONG\r\n3\r\n")
+    sys.stdout.write('c1 ' + c1.read_some())
+    c1.write("PONG\r\n7\r\n")
+    sys.stdout.write('c2 ' + c2.read_some())
+    sys.stdout.write('c1 ' + c1.read_some())
+    sys.stdout.write('c2 ' + c2.read_some())
+    sys.stdout.write('c1 ' + c1.read_some())
 
     c1.close()
     c2.close()
